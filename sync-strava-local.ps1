a654e36ef88ededbfb7abaa0597fb5b908f9a9ac
Niewei <neewii.yang@outlook.com>
2026-05-28 10:22:28 +0800

    更新本地同步脚本

--- sync-strava-local.ps1
+++ sync-strava-local.ps1
@@ -38,8 +38,90 @@ if ($missingVars.Count -gt 0) {
     throw "Please fill these values in .env first: $($missingVars -join ', ')"
 }
 
+function Get-EnvOrDefault {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string] $Name,
+        [Parameter(Mandatory = $true)]
+        [string] $Default
+    )
+
+    $value = [Environment]::GetEnvironmentVariable($Name, "Process")
+    if ([string]::IsNullOrWhiteSpace($value)) {
+        return $Default
+    }
+    return $value
+}
+
+$title = Get-EnvOrDefault -Name "TITLE" -Default "Workouts"
+$titleGrid = Get-EnvOrDefault -Name "TITLE_GRID" -Default "Over 10km Workouts"
+$athlete = Get-EnvOrDefault -Name "ATHLETE" -Default "Neewii"
+$minGridDistance = Get-EnvOrDefault -Name "MIN_GRID_DISTANCE" -Default "10"
+$birthdayMonth = Get-EnvOrDefault -Name "BIRTHDAY_MONTH" -Default "2000-06"
+$currentYear = Get-Date -Format "yyyy"
+
 python "$PSScriptRoot\run_page\strava_sync.py" `
     $env:STRAVA_CLIENT_ID `
     $env:STRAVA_CLIENT_SECRET `
     $env:STRAVA_CLIENT_REFRESH_TOKEN `
     @SyncArgs
+
+python "$PSScriptRoot\run_page\gen_svg.py" `
+    --from-db `
+    --title "$title" `
+    --type github `
+    --github-style align-firstday `
+    --athlete "$athlete" `
+    --special-distance 10 `
+    --special-distance2 20 `
+    --special-color yellow `
+    --special-color2 red `
+    --output "$PSScriptRoot\assets\github.svg" `
+    --use-localtime `
+    --min-distance 0.1
+
+python "$PSScriptRoot\run_page\gen_svg.py" `
+    --from-db `
+    --title "$titleGrid" `
+    --type grid `
+    --athlete "$athlete" `
+    --output "$PSScriptRoot\assets\grid.svg" `
+    --special-color yellow `
+    --special-color2 red `
+    --special-distance 20 `
+    --special-distance2 40 `
+    --use-localtime `
+    --min-distance "$minGridDistance"
+
+python "$PSScriptRoot\run_page\gen_svg.py" `
+    --from-db `
+    --type circular `
+    --use-localtime
+
+python "$PSScriptRoot\run_page\gen_svg.py" `
+    --from-db `
+    --year "$currentYear" `
+    --language zh_CN `
+    --title "$currentYear Workouts" `
+    --type github `
+    --github-style align-firstday `
+    --athlete "$athlete" `
+    --special-distance 10 `
+    --special-distance2 20 `
+    --special-color yellow `
+    --special-color2 red `
+    --output "$PSScriptRoot\assets\github_$currentYear.svg" `
+    --use-localtime `
+    --min-distance 0.1
+
+python "$PSScriptRoot\run_page\gen_svg.py" `
+    --from-db `
+    --type monthoflife `
+    --birth "$birthdayMonth" `
+    --special-color "#f9d367" `
+    --special-color2 "#f0a1a8" `
+    --output "$PSScriptRoot\assets\mol.svg" `
+    --use-localtime `
+    --athlete "$athlete" `
+    --title "Month of Life" `
+    --sport-type all
